fc44ea392634a9322ea3c1f13ea1b910b90cb979
juvairiyapk1 <juvairiyapk777@gmail.com>
2023-10-23 16:27:17 +0530

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: iPortfolio
-* Updated: May 30 2023 with Bootstrap v5.3.0
-* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
@@ -189,58 +183,6 @@
 
   });
 
-  /**
-   * Initiate portfolio lightbox 
-   */
-  // const portfolioLightbox = GLightbox({
-    // selector: '.portfolio-lightbox'
-  // });
-
-  /**
-   * Portfolio details slider
-   */
-  // new Swiper('.portfolio-details-slider', {
-    // speed: 400,
-    // loop: true,
-    // autoplay: {
-      // delay: 5000,
-      // disableOnInteraction: false
-    // },
-    // pagination: {
-      // el: '.swiper-pagination',
-      // type: 'bullets',
-      // clickable: true
-    // }
-  // });
-
-  /**
-   * Testimonials slider
-   */
-  // new Swiper('.testimonials-slider', {
-    // speed: 600,
-    // loop: true,
-    // autoplay: {
-      // delay: 5000,
-      // disableOnInteraction: false
-    // },
-    // slidesPerView: 'auto',
-    // pagination: {
-      // el: '.swiper-pagination',
-      // type: 'bullets',
-      // clickable: true
-    // },
-    // breakpoints: {
-      // 320: {
-        // slidesPerView: 1,
-        // spaceBetween: 20
-      // },
-
-      // 1200: {
-        // slidesPerView: 3,
-        // spaceBetween: 20
-      // }
-    // }
-  // });
 
   /**
    * Animation on scroll
